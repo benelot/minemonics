4410fd56020bf86147828b7949e53a706a664b85
Benjamin Ellenberger <be.ellenberger@gmail.com>
2014-11-17 11:02:15 +0100

    Initial structure of the Minemonics - Virtual Creature Evolution Simulator. TestingOgre is an example project and will soon be moved to a folder for prototypes. The project follows

---
+++ Minemonics/src/TestingOgre/TutorialApplication.h
@@ -0,0 +1,32 @@
+/*
+-----------------------------------------------------------------------------
+Filename:    TutorialApplication.h
+-----------------------------------------------------------------------------
+
+This source file is part of the
+   ___                 __    __ _ _    _ 
+  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
+ //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
+/ \_// (_| | | |  __/  \  /\  /| |   <| |
+\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+      |___/                              
+      Tutorial Framework
+      http://www.ogre3d.org/tikiwiki/
+-----------------------------------------------------------------------------
+*/
+#ifndef __TutorialApplication_h_
+#define __TutorialApplication_h_
+
+#include "BaseApplication.h"
+
+class TutorialApplication : public BaseApplication
+{
+public:
+    TutorialApplication(void);
+    virtual ~TutorialApplication(void);
+
+protected:
+    virtual void createScene(void);
+};
+
+#endif // #ifndef __TutorialApplication_h_
---
+++ Minemonics/src/TestingOgre/build/CMakeFiles/OgreApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/OgreApp.dir/BaseApplication.cpp.o"
+  "CMakeFiles/OgreApp.dir/TutorialApplication.cpp.o"
+  "dist/bin/OgreApp.pdb"
+  "dist/bin/OgreApp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/OgreApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
