34db59437c4083083b02efb33f73cc518f1c2647
Benjamin Ellenberger <be.ellenberger@gmail.com>
2015-02-26 13:05:14 +0100

    Remove the model settings and merge them into the main configuration.

---
+++ Minemonics/src/configuration/ControlConfiguration.h
@@ -0,0 +1,14 @@
+/*
+ * ControlConfiguration.h
+ *
+ *  Created on: Feb 26, 2015
+ *      Author: leviathan
+ */
+
+#ifndef CONTROLCONFIGURATION_H_
+#define CONTROLCONFIGURATION_H_
+
+class ControlConfiguration {
+};
+
+#endif /* CONTROLCONFIGURATION_H_ */
---
+++ Minemonics/src/configuration/MorphologyConfiguration.h
@@ -0,0 +1,18 @@
+/*
+ * MorphologyConfiguration.h
+ *
+ *  Created on: Feb 26, 2015
+ *      Author: leviathan
+ */
+
+#ifndef MORPHOLOGYCONFIGURATION_H_
+#define MORPHOLOGYCONFIGURATION_H_
+
+
+class MorphologyConfiguration {
+public:
+
+};
+
+
+#endif /* MORPHOLOGYCONFIGURATION_H_ */
