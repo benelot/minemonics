af0d2045c0c3506ac7d7afecfc218961d23b42d8
Benjamin Ellenberger <be.ellenberger@gmail.com>
2015-03-31 13:54:21 +0200

    Cleanup cmake files.

---
+++ Minemonics/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/leviathan/git/minemonics/Minemonics/src
+# Build directory: /home/leviathan/git/minemonics/Minemonics/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(runUnitTests "/home/leviathan/git/minemonics/Minemonics/build/dist/bin/runUnitTests")
