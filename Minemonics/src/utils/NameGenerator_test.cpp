42f66bc60c707d2d561da878419e3795108f720b
Benjamin Ellenberger <be.ellenberger@gmail.com>
2015-07-21 13:37:45 +0200

    NameGenerator is not completely unique in the name generation, but is ok for the moment.

--- Minemonics/src/utils/NameGenerator_test.cpp
+++ Minemonics/src/utils/NameGenerator_test.cpp
@@ -20,12 +20,17 @@
 //## view headers
 //## utils headers
 
+#define NUM_NAMES 500
+
 class NameGeneratorTest: public ::testing::Test {
 protected:
 	virtual void SetUp() {
+		//initialize the randomness singleton
+		randomness = new Randomness();
+
 		nameGenerator = new NameGenerator();
 
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < NUM_NAMES; i++) {
 			names.push_back(nameGenerator->generateFirstName());
 		}
 
@@ -34,15 +39,19 @@ protected:
 	virtual void TearDown() {
 		delete nameGenerator;
 		nameGenerator = 0;
+		delete randomness;
+		randomness = 0;
 	}
+	Randomness* randomness;
 	NameGenerator* nameGenerator;
+
 	std::vector<std::string> names;
 };
 
 TEST_F(NameGeneratorTest,namesAreUnique) {
-	for (int i = 0; i < 100; i++) {
-		//std::cout << names.at(i) << std::endl;
-		for (int j = 0; j < 100; j++) {
+	for (int i = 0; i < NUM_NAMES; i++) {
+//		std::cout << names.at(i) << std::endl;
+		for (int j = 0; j < NUM_NAMES; j++) {
 			if (i != j) {
 				ASSERT_TRUE(names.at(i) != names.at(j));
 			}
