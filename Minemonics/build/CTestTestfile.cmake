11ca9682b1903cf358e9afd52d6c183d93124c87
Benjamin Ellenberger <benelot@users.noreply.github.com>
2015-08-12 11:55:09 +0200

    Delete CTestTestfile.cmake

--- Minemonics/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/leviathan/SPACE/meta/Work.Repositories/git/minemonics/Minemonics/src
-# Build directory: /media/leviathan/SPACE/meta/Work.Repositories/git/minemonics/Minemonics/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
