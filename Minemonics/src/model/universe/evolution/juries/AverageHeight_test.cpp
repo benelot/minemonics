1fd64fed81e406a63f62969e151690a8620db084
Benjamin Ellenberger <be.ellenberger@gmail.com>
2015-08-19 12:19:30 +0200

    Add averageheight test.

--- Minemonics/src/model/universe/evolution/juries/AverageHeight_test.cpp
+++ Minemonics/src/model/universe/evolution/juries/AverageHeight_test.cpp
@@ -1,9 +1,41 @@
-/*
- * AverageHeight_test.cpp
- *
- *  Created on: Apr 2, 2015
- *      Author: leviathan
- */
-
+//# corresponding header
+#include <gtest/gtest.h>
 #include <model/universe/evolution/juries/AverageHeight.hpp>
+#include <iostream>
+
+//## controller headers
+//## model headers
+//## view headers
+//# custom headers
+//## base headers
+//## configuration headers
+//## controller headers
+//## model headers
+//## view headers
+//## utils headers
+
+class AverageHeightTest: public ::testing::Test {
+protected:
+	virtual void SetUp() {
+		height = new AverageHeight(true, 1);
+	}
+
+	virtual void TearDown() {
+		delete height;
+		height = 0;
+	}
+	AverageHeight* height;
+};
+
+TEST_F(AverageHeightTest,hasWeightOne) {
+	ASSERT_TRUE(height->getWeight() == 1);
+}
+
+TEST_F(AverageHeightTest,evaluateHeight) {
+	//TODO: Broken test
+//	velocity->calculateFitness(0,0,0,0);
+//	velocity->calculateFitness(1,0,0,1);
+//	velocity->evaluateFitness();
+//	ASSERT_TRUE(velocity->getFitness() == 1);
+}
 
