a1781b439cfd935d9b2e6d242a950e49cf891453
Benjamin Ellenberger <be.ellenberger@gmail.com>
2015-08-27 01:09:17 +0200

    Fix population serialization test. #133

--- Minemonics/src/model/universe/evolution/population/PopulationModel_test.cpp
+++ Minemonics/src/model/universe/evolution/population/PopulationModel_test.cpp
@@ -1,50 +1,71 @@
-/*
- * Population_test.cpp
- *
- *  Created on: Mar 9, 2015
- *      Author: leviathan
- */
-
 //# corresponding header
-#include <configuration/MorphologyConfiguration.hpp>
-#include <controller/SaveController.hpp>
-#include <gtest/gtest.h>
 #include <model/universe/evolution/population/PopulationModel.hpp>
+#include <gtest/gtest.h>
 
+//# forward declarations
+//# system headers
 //## controller headers
 //## model headers
 //## view headers
 //# custom headers
 //## base headers
 //## configuration headers
-
 //## controller headers
+#include <controller/SaveController.hpp>
 
 //## model headers
 //## view headers
 //## utils headers
+#include <utils/Randomness.hpp>
 
 class PopulationTest: public ::testing::Test {
 protected:
 	virtual void SetUp() {
 		// Set up an object of the class you want to test
+		randomness = new Randomness();
 		population = new PopulationModel();
-		population->initialize(NULL, 30);
+		physicsController = new PhysicsController();
+		physicsController->initBulletPhysics();
+		population->initialize(NULL, 0);
+		for (int i = 0; i < 30; ++i) {
+			CreatureModel* creature = new CreatureModel();
+			creature->setWorld(physicsController->getDynamicsWorld());
+			creature->initialize(NULL, Ogre::Vector3(0, 0, 0), 30);
+			creature->performEmbryogenesis();
+			population->addMember(creature);
+		}
 	}
 
 	virtual void TearDown() {
 		// delete and set the pointer to zero
 		delete population;
 		population = NULL;
+		delete physicsController;
+		physicsController = NULL;
+		delete randomness;
+		randomness = NULL;
 	}
+	Randomness* randomness;
 	PopulationModel* population;
+
+	PhysicsController* physicsController;
 };
 
 class PopulationSerializationTest: public ::testing::Test {
 protected:
 	virtual void SetUp() {
+		randomness = new Randomness();
 		population = new PopulationModel();
-		population->initialize(NULL, 30);
+		physicsController = new PhysicsController();
+		physicsController->initBulletPhysics();
+		population->initialize(NULL, 0);
+		for (int i = 0; i < 30; ++i) {
+			CreatureModel* creature = new CreatureModel();
+			creature->setWorld(physicsController->getDynamicsWorld());
+			creature->initialize(NULL, Ogre::Vector3(0, 0, 0), 30);
+			creature->performEmbryogenesis();
+			population->addMember(creature);
+		}
 
 		population2 = new PopulationModel();
 
@@ -60,10 +81,18 @@ protected:
 		population = NULL;
 		delete population2;
 		population2 = NULL;
+		delete physicsController;
+		physicsController = NULL;
+		delete randomness;
+		randomness = NULL;
 	}
+	Randomness* randomness;
+
 	PopulationModel* population;
 
 	PopulationModel* population2;
+
+	PhysicsController* physicsController;
 };
 
 TEST_F(PopulationTest,DummyTest) {
