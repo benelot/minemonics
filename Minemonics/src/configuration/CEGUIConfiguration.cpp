244b68dd1e5a0b0197821f4008d972ceafdbd533
Benjamin Ellenberger <be.ellenberger@gmail.com>
2015-08-31 19:08:48 +0200

    Add configuration file.

---
+++ Minemonics/src/configuration/CEGUIConfiguration.cpp
@@ -0,0 +1,17 @@
+//# corresponding headers
+#include <configuration/CEGUIConfiguration.hpp>
+
+//# forward declarations
+//# system headers
+//## controller headers
+//## model headers
+//## view headers
+//# custom headers
+//## base headers
+//## configuration headers
+//## controller headers
+//## model headers
+//## view headers
+//## utils headers
+
+const std::string CEGUIConfiguration::CEGUI_SCHEME = "Ogremonics";
