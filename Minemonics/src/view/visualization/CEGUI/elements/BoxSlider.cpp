5a9aec051a80f3d51e151d839ca25a111044b9d3
Benjamin Ellenberger <be.ellenberger@gmail.com>
2015-08-31 11:59:58 +0200

    Add BoxSlider as a custom element with an editbox and a slider combined.

---
+++ Minemonics/src/view/visualization/CEGUI/elements/BoxSlider.cpp
@@ -0,0 +1,60 @@
+/*
+ * BoxSlider.cpp
+ *
+ *  Created on: Aug 29, 2015
+ *      Author: leviathan
+ */
+
+#include <boost/lexical_cast.hpp>
+#include <configuration/CEGUIConfiguration.hpp>
+#include <CEGUI/Element.h>
+#include <CEGUI/Singleton.h>
+#include <CEGUI/Size.h>
+#include <CEGUI/UDim.h>
+#include <CEGUI/Vector.h>
+#include <CEGUI/widgets/Slider.h>
+#include <CEGUI/WindowManager.h>
+#include <view/visualization/CEGUI/elements/BoxSlider.hpp>
+#include <string>
+
+BoxSlider::BoxSlider(const std::string name, const int width,
+	const std::string validationString) :
+	CEGUI::Window(CEGUIConfiguration::CEGUI_SCHEME + "/Slider", name) {
+	setSize(CEGUI::USize(CEGUI::UDim(0, width), CEGUI::UDim(0, 30)));
+	CEGUI::WindowManager& wmgr = CEGUI::WindowManager::getSingleton();
+
+	mSlider = static_cast<CEGUI::Slider*>(wmgr.createWindow(
+		CEGUIConfiguration::CEGUI_SCHEME + "/Slider"));
+	mSlider->setMaxValue(100.0f);
+	mSlider->setClickStep(1.0f);
+	mSlider->setCurrentValue(10.0f);
+	mSlider->subscribeEvent(CEGUI::Slider::EventValueChanged,
+		CEGUI::Event::Subscriber(&BoxSlider::onSliderValueChanged, this));
+//	float valueSlider = slider->getCurrentValue(); // Retrieve the value
+	mSlider->setSize(CEGUI::USize(CEGUI::UDim(0, 120), CEGUI::UDim(0, 20)));
+	mSlider->setPosition(
+		CEGUI::UVector2(CEGUI::UDim(0.0, 0.0f), CEGUI::UDim(0.0, 0.0f)));
+	addChild (mSlider);
+
+	mEditBox = static_cast<CEGUI::Editbox*>(wmgr.createWindow(
+		CEGUIConfiguration::CEGUI_SCHEME + "/Editbox"));
+	mEditBox->setText(
+		boost::lexical_cast < std::string > (mSlider->getCurrentValue()));
+//	CEGUI::String valueEditbox = mEditBox->getText(); // Retrieve the text
+	mEditBox->setValidationString(validationString);
+	mEditBox->setSize(CEGUI::USize(CEGUI::UDim(0, 50), CEGUI::UDim(0, 20)));
+	mEditBox->setPosition(
+		CEGUI::UVector2(CEGUI::UDim(0.6f, 0.0f), CEGUI::UDim(0.0, 0.0f)));
+	addChild (mEditBox);
+
+}
+
+BoxSlider::~BoxSlider() {
+	// TODO Auto-generated destructor stub
+}
+
+void BoxSlider::onSliderValueChanged() {
+	mEditBox->setText(
+		boost::lexical_cast < std::string > (mSlider->getCurrentValue()));
+}
+
---
+++ Minemonics/src/view/visualization/CEGUI/elements/BoxSlider.hpp
@@ -0,0 +1,37 @@
+/*
+ * BoxSlider.hpp
+ *
+ *  Created on: Aug 29, 2015
+ *      Author: leviathan
+ */
+
+#ifndef VIEW_VISUALIZATION_CEGUI_ELEMENTS_BOXSLIDER_HPP_
+#define VIEW_VISUALIZATION_CEGUI_ELEMENTS_BOXSLIDER_HPP_
+
+#include <CEGUI/String.h>
+#include <CEGUI/widgets/DefaultWindow.h>
+#include <CEGUI/widgets/Editbox.h>
+#include <CEGUI/Window.h>
+#include <CEGUI/widgets/GroupBox.h>
+#include <boost/lexical_cast.hpp>
+
+/*
+ *
+ */
+class BoxSlider: public CEGUI::Window {
+public:
+	BoxSlider(const std::string name, const int width,
+		const std::string validationString);
+	virtual ~BoxSlider();
+
+	void onSliderValueChanged();
+
+	float getCurrentValue() {
+		return boost::lexical_cast<int>(mEditBox->getText());
+	}
+private:
+	CEGUI::Slider* mSlider;
+	CEGUI::Editbox* mEditBox;
+};
+
+#endif /* VIEW_VISUALIZATION_CEGUI_ELEMENTS_BOXSLIDER_HPP_ */
