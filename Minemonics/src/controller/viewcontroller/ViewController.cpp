64e2e2b6a1104d82977f5887c45a84a872bc6277
Benjamin Ellenberger <be.ellenberger@gmail.com>
2015-09-18 19:02:42 +0200

    Add mNewPlanetWindow to the initializer list.

--- Minemonics/src/controller/viewcontroller/ViewController.cpp
+++ Minemonics/src/controller/viewcontroller/ViewController.cpp
@@ -47,7 +47,7 @@ ViewController::_Init ViewController::_initializer;
 ViewController::ViewController() :
 	mRenderer(NULL), mLayout(NULL), mSystem(NULL), mDragContainer(NULL), mEvaluationInView(
 		NULL), mShowShadows(false), mSelectedPlanet(NULL), mMenuBar(NULL), mFpsPanel(
-		NULL), mDetailsPanel(NULL) {
+		NULL), mDetailsPanel(NULL), mNewPlanetWindow(NULL) {
 }
 
 ViewController::~ViewController() {
