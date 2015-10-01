dd8c2a8309b9f3c1944a538b3b8a89a027d76c91
Benjamin Ellenberger <be.ellenberger@gmail.com>
2015-10-01 15:25:26 +0200

    Fix box slider to behave normally and be of definable length.

--- Minemonics/src/view/visualization/CEGUI/elements/BoxSlider.cpp
+++ Minemonics/src/view/visualization/CEGUI/elements/BoxSlider.cpp
@@ -1,12 +1,13 @@
-/*
- * BoxSlider.cpp
- *
- *  Created on: Aug 29, 2015
- *      Author: leviathan
- */
+//# corresponding headers
+//# forward declarations
+//# system headers
+#include <string>
 
+//## controller headers
+//## model headers
 #include <boost/lexical_cast.hpp>
-#include <configuration/CEGUIConfiguration.hpp>
+
+//## view headers
 #include <CEGUI/Element.h>
 #include <CEGUI/Singleton.h>
 #include <CEGUI/Size.h>
@@ -14,35 +15,55 @@
 #include <CEGUI/Vector.h>
 #include <CEGUI/widgets/Slider.h>
 #include <CEGUI/WindowManager.h>
+
+//# custom headers
+//## base headers
+//## configuration headers
+#include <configuration/CEGUIConfiguration.hpp>
+
+//## controller headers
+//## model headers
+//## view headers
 #include <view/visualization/CEGUI/elements/BoxSlider.hpp>
-#include <string>
+
+//## utils headers
 
 BoxSlider::BoxSlider(const std::string name, const int width,
 	const std::string validationString) :
 	CEGUI::Window(CEGUIConfiguration::CEGUI_SCHEME + "/Slider", name) {
-	setSize(CEGUI::USize(CEGUI::UDim(0, width), CEGUI::UDim(0, 30)));
+	setSize(CEGUI::USize(CEGUI::UDim(0, width), CEGUI::UDim(0, 20)));
 	CEGUI::WindowManager& wmgr = CEGUI::WindowManager::getSingleton();
 
+	double editBoxWidth = 50;
 	mSlider = static_cast<CEGUI::Slider*>(wmgr.createWindow(
-		CEGUIConfiguration::CEGUI_SCHEME + "/Slider"));
+		CEGUIConfiguration::CEGUI_SCHEME + "/HorizontalSlider"));
+
+	double sliderLength = 0.6f;
+
+	mSlider->setSize(
+		CEGUI::USize(CEGUI::UDim(0, sliderLength * width), CEGUI::UDim(0, 20)));
+
+	mSlider->setPosition(
+		CEGUI::UVector2(CEGUI::UDim(0, 0.0f), CEGUI::UDim(0, 0.0f)));
+
 	mSlider->setMaxValue(100.0f);
 	mSlider->setClickStep(1.0f);
 	mSlider->setCurrentValue(10.0f);
+
 	mSlider->subscribeEvent(CEGUI::Slider::EventValueChanged,
 		CEGUI::Event::Subscriber(&BoxSlider::onSliderValueChanged, this));
-//	float valueSlider = slider->getCurrentValue(); // Retrieve the value
-	mSlider->setSize(CEGUI::USize(CEGUI::UDim(0, 120), CEGUI::UDim(0, 20)));
-	mSlider->setPosition(
-		CEGUI::UVector2(CEGUI::UDim(0.0, 0.0f), CEGUI::UDim(0.0, 0.0f)));
+
 	addChild (mSlider);
 
 	mEditBox = static_cast<CEGUI::Editbox*>(wmgr.createWindow(
 		CEGUIConfiguration::CEGUI_SCHEME + "/Editbox"));
 	mEditBox->setText(
-		boost::lexical_cast < std::string > (mSlider->getCurrentValue()));
+		boost::lexical_cast < std::string
+			> (floor(mSlider->getCurrentValue())));
 //	CEGUI::String valueEditbox = mEditBox->getText(); // Retrieve the text
 	mEditBox->setValidationString(validationString);
-	mEditBox->setSize(CEGUI::USize(CEGUI::UDim(0, 50), CEGUI::UDim(0, 20)));
+	mEditBox->setSize(
+		CEGUI::USize(CEGUI::UDim(0, editBoxWidth), CEGUI::UDim(0, 20)));
 	mEditBox->setPosition(
 		CEGUI::UVector2(CEGUI::UDim(0.6f, 0.0f), CEGUI::UDim(0.0, 0.0f)));
 	addChild (mEditBox);
@@ -50,11 +71,12 @@ BoxSlider::BoxSlider(const std::string name, const int width,
 }
 
 BoxSlider::~BoxSlider() {
-	
+
 }
 
 void BoxSlider::onSliderValueChanged() {
 	mEditBox->setText(
-		boost::lexical_cast < std::string > (mSlider->getCurrentValue()));
+		boost::lexical_cast < std::string
+			> (floor(mSlider->getCurrentValue())));
 }
 
