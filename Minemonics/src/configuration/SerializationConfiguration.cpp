77dd04f1b6cb83096a6512facf99aa8e4c80d881
Benjamin Ellenberger <be.ellenberger@gmail.com>
2015-10-09 16:17:37 +0200

    Check in long missing SerializationConfiguration file.

---
+++ Minemonics/src/configuration/SerializationConfiguration.cpp
@@ -0,0 +1,18 @@
+//# corresponding headers
+#include <configuration/SerializationConfiguration.hpp>
+
+//# forward declarations
+//# system headers
+//## controller headers
+//## model headers
+//## view headers
+//# custom headers
+//## base headers
+//## configuration headers
+//## controller headers
+//## model headers
+//## view headers
+//## utils headers
+
+
+const std::string SerializationConfiguration::TOP_FOLDER = "universe";
