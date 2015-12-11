007dea68b522807e3f310ae56613448f5eacda7b
Benjamin Ellenberger <be.ellenberger@gmail.com>
2015-12-11 19:25:18 +0100

    Fix wrong descriptions. Rerun the experiment with the other desc.
    --------------------------------------
    Leaving the x variable free makes the trajectory not stay in the plane.

--- Minemonics/src/model/universe/evolution/population/creature/phenome/controller/chaotic/ChaoticController.cpp
+++ Minemonics/src/model/universe/evolution/population/creature/phenome/controller/chaotic/ChaoticController.cpp
@@ -91,7 +91,7 @@ void ChaoticController::perform(const double timeSinceLastTick) {
 
 	if (mControlInputs.size() >= 2 && !mFirstTime) {
 		// set the inputs to the chaotic controller
-//		u[0] = mControlInputs[0]->getOutputValue();
+		u[0] = mControlInputs[0]->getOutputValue();
 //		u[0] = mControlInputs[1]->getOutputValue();
 
 //		u[1] = mControlInputs[0]->getOutputValue();
