afab0bf16b319bb38f2031410e88a0ba3c0715eb
Benjamin Ellenberger <be.ellenberger@gmail.com>
2016-01-07 16:24:50 +0100

    Update application version number.

--- Minemonics/src/configuration/ApplicationConfiguration.cpp
+++ Minemonics/src/configuration/ApplicationConfiguration.cpp
@@ -15,7 +15,7 @@
 //## utils headers
 
 const std::string ApplicationConfiguration::APPLICATION_TITLE = /**!< version.features.bugs-fixed.commit */
-"Minemonics - The Origin of Creatures v.0.73.19.870"; /**!< The title of the application.*/
+"Minemonics - The Origin of Creatures v.0.74.22.910"; /**!< The title of the application.*/
 
 const int ApplicationConfiguration::APPLICATION_TICK = 1000.0f / 60.0f; //ms
 
