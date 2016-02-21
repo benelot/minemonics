b50f3640e5630a53814ab3424d4e523edc19edb4
Benjamin Ellenberger <be.ellenberger@gmail.com>
2016-02-21 18:08:33 +0100

    Fix bibliography citations. compile with compile.sh for a properly compiled document in build/main.pdf

--- Minemonics/doc/reports/Thesis/compile.sh
+++ Minemonics/doc/reports/Thesis/compile.sh
@@ -2,3 +2,5 @@
 pdflatex main.tex
 biber main.bcf
 pdflatex main.tex
+mv main.toc main.thm main.tdo main.run.xml main.out main.lot main.log main.lof main.bcf main.aux main.blg main.bbl main.pdf --target-directory=build
+evince build/main.pdf&
