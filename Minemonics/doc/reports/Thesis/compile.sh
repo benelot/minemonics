da7dcfe1205e8c962fec9bcbc2f7e70153b4e775
Benjamin Ellenberger <be.ellenberger@gmail.com>
2016-04-29 17:37:18 +0200

    Correct all issues discussed with Karlis.

--- Minemonics/doc/reports/Thesis/compile.sh
+++ Minemonics/doc/reports/Thesis/compile.sh
@@ -2,5 +2,6 @@
 pdflatex main.tex
 biber main.bcf
 pdflatex main.tex
+pdflatex main.tex
 mv main.toc main.thm main.tdo main.run.xml main.out main.lot main.log main.lof main.bcf main.aux main.blg main.bbl main.pdf --target-directory=build
 evince build/main.pdf&
